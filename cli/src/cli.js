d0f6ff24e15bc02ce4a64222075b3306ed41309c
Trey Howle <howleg@email.uscupstate.edu>
2016-10-20 17:23:04 -0500

    Revert "ui with options for port and host working"
    This reverts commit 0a1b7ebe53b1023af45e2b2e9c666c524dd9426d.

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -14,22 +14,11 @@ cli
   .delimiter(cli.chalk['yellow']('ftd~$'))
 
 cli
-  .mode('connect <username> [h] [p]')
-  
+  .mode('connect <username>')
   .delimiter(cli.chalk['green']('connected>'))
   .init(function (args, callback) {
-	  
-	  let username = args.username
-      let port = 8080
-      let host = 'localhost'
-    	  
-    	  
-      if (args.options.port != undefined && args.options.host != undefined) {
-          host = args.options.h
-          port = args.options.p
-      }
-	  
-    server = connect({ host: host, port: port }, () => {
+    username = args.username
+    server = connect({ host: 'localhost', port: 8080 }, () => {
       server.write(new Message({ username, command: 'connect' }).toJSON() + '\n')
       callback()
     })
@@ -93,7 +82,13 @@ cli
       server.end(new Message({ username, command }).toJSON() + '\n')
       
     } else if (cmd === 'echo') {
-    	prevCmd = command;	
+    	prevCmd = command;
+    	
+    	console.log('1st echo')
+   	 	console.log(username)
+   	 	console.log(command)
+   	 	console.log(contents)
+    	
       server.write(new Message({ username, command, contents }).toJSON() + '\n')
     } 
     
@@ -103,7 +98,13 @@ cli
       } 
     
     else if (cmd === 'broadcast') {
-    	prevCmd = command;	
+    	prevCmd = command;
+    	
+    	console.log('1st broadcast')
+   	 	console.log(username)
+   	 	console.log(command)
+   	 	console.log(contents)
+    	
     	server.write(new Message({ username, command, contents }).toJSON() + '\n')	
     }
     
